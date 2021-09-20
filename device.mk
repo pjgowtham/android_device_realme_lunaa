b0b5e44f69451e7836450ac40ca16f8ec1f310db
Bruno Martins <bgcngm@gmail.com>
2021-09-20 18:23:58 +0100

    lemonade: Blob out ODM feature list
    There's not much of a purpose to have this in the device tree.
    
    Change-Id: I67751642b2f34576106a39a83e21996010215c3d

--- device.mk
+++ device.mk
@@ -42,10 +42,6 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina/mixer_paths.xml
 
-# OPFeature
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/odm_feature_list:$(TARGET_COPY_OUT_ODM)/etc/odm_feature_list
-
 PRODUCT_SHIPPING_API_LEVEL := 30
 
 # Inherit from oneplus sm8350-common
