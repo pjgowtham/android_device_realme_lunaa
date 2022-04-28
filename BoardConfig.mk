4a2383d9a3e4bd1a056a4077a4ec9c4504212e65
pjgowtham <pjgowtham@gmail.com>
2022-04-28 12:44:05 +0530

    RMX3360: Rebrand to GT Master Edition

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,10 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Include the common OEM chipset BoardConfig.
-include device/oneplus/sm8350-common/BoardConfigCommon.mk
+DEVICE_PATH := device/oplus/RMX3360
 
-DEVICE_PATH := device/oneplus/lemonade
+# Include the common OEM chipset BoardConfig.
+include device/oplus/sm8350-common/BoardConfigCommon.mk
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
@@ -22,4 +22,4 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 TARGET_RECOVERY_UI_MARGIN_HEIGHT := 103
 
 # Include the proprietary files BoardConfig.
-include vendor/oneplus/lemonade/BoardConfigVendor.mk
+include vendor/oplus/RMX3360/BoardConfigVendor.mk
