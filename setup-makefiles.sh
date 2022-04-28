7b7a773ecf40867faa403666c2a970a25d4f8599
pjgowtham <pjgowtham@gmail.com>
2022-04-28 12:44:05 +0530

    lunaa: Initial device tree conversion

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -8,8 +8,8 @@
 
 set -e
 
-export DEVICE=lemonade
+export DEVICE=lunaa
 export DEVICE_COMMON=sm8350-common
-export VENDOR=oneplus
+export VENDOR=oplus
 
 "./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
