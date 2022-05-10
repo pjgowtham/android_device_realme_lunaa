a641dddae7e5de8ddcf618bd4f89dabe1daae431
Alexander Koskovich <akoskovich@pm.me>
2022-05-09 20:15:29 -0700

    lemonade: Don't pick up makefiles under local path
     * There are no more makefiles to pick up.
    
    Change-Id: Icdda4aa8af70c849b870468cdd38bf5e73351df0

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021-2022 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -26,7 +26,3 @@
 #
 
 LOCAL_PATH := $(call my-dir)
-
-ifeq ($(TARGET_DEVICE),lemonade)
-include $(call all-makefiles-under,$(LOCAL_PATH))
-endif
