fcfbf08ca4f37e41f3ce544efdcf1138d8fbff04
Alexander Koskovich <akoskovich@pm.me>
2022-05-18 08:58:30 -0700

    lemonade: Drop virtual A/B inherit, duplicate
    Change-Id: If11e7d7724ce6848fa17780254edcd85c706e1be

--- device.mk
+++ device.mk
@@ -22,9 +22,6 @@
 # definition file).
 #
 
-# Enable virtual A/B OTA
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
