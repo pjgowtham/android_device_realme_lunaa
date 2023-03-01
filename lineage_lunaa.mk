DhRV00T/device_oplus_RMX3461

lunaa: Initial device tree conversion

lineage_lunaa.mk
@@ -1,0 +1,32 @@
+#
+# Copyright (C) 2021-2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from lunaa device
+$(call inherit-product, device/oplus/lunaa/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_lunaa
+PRODUCT_DEVICE := lunaa
+PRODUCT_MANUFACTURER := realme
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := RMX3360
+
+PRODUCT_SYSTEM_NAME := RMX3360
+PRODUCT_SYSTEM_DEVICE := RE54ABL1
+
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="RMX3360-user 13 TP1A.220905.001 R.c87188-d1f_1b5 release-keys" \
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+
+BUILD_FINGERPRINT := realme/RMX3360/RE54ABL1:13/TP1A.220905.001/R.c87188-d1f_1b5:user/release-keys

pjgowtham/android_device_realme_lunaa

Change-Id: If0bf43f9962b0a60f94e719b302db5e1b94497f7

lineage_lunaa.mk
@@ -1,0 +1,32 @@
+#
+# Copyright (C) 2021-2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from lunaa device
+$(call inherit-product, device/realme/lunaa/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_lunaa
+PRODUCT_DEVICE := lunaa
+PRODUCT_MANUFACTURER := realme
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := RMX3360
+
+PRODUCT_SYSTEM_NAME := RMX3360
+PRODUCT_SYSTEM_DEVICE := RE54ABL1
+
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="RMX3360-user 13 TP1A.220905.001 R.c87188-d1f_1b5 release-keys" \
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+
+BUILD_FINGERPRINT := realme/RMX3360/RE54ABL1:13/TP1A.220905.001/R.c87188-d1f_1b5:user/release-keys