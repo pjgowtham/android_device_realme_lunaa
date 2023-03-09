0c680657594122b2b9acafd359e6493a777d3f15
pjgowtham <pjgowtham@gmail.com>
2023-03-09 11:01:33 +0530

    lunaa: Initial device tree conversion
    * OTA assertion is matched with stock (RE54ABL1)
    * Add VENDOR_COMMON to extraction scripts
    * Set super partition size and group size to BOARD_SUPER_PARTITION_SIZE - 4MB s>
    * Include additional defconfig for oplus yupik
    
    Change-Id: If0bf43f9962b0a60f94e719b302db5e1b94497f7

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2023 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lemonade.mk
+    $(LOCAL_DIR)/lineage_lunaa.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lemonade-user \
-    lineage_lemonade-userdebug \
-    lineage_lemonade-eng
+    lineage_lunaa-user \
+    lineage_lunaa-userdebug \
+    lineage_lunaa-eng
