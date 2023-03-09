204bee536227430656cd953fcd587aa4c19d6c02
pjgowtham <pjgowtham@gmail.com>
2023-03-09 11:01:33 +0530

    lunaa: Initial device tree conversion
    * OTA assertion is matched with stock (RE54ABL1)
    * Add VENDOR_COMMON to extraction scripts
    * Set super partition size and group size to BOARD_SUPER_PARTITION_SIZE - 4MB s>
    * Include additional defconfig for oplus yupik
    
    Change-Id: If0bf43f9962b0a60f94e719b302db5e1b94497f7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021-2023 The LineageOS Project
+# Copyright (C) 2023 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -7,13 +7,20 @@
 # Include the common OEM chipset BoardConfig.
 include device/oneplus/sm8350-common/BoardConfigCommon.mk
 
-DEVICE_PATH := device/oneplus/lemonade
+DEVICE_PATH := device/realme/lunaa
 
 # Display
 TARGET_SCREEN_DENSITY := 450
 
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
+
+# Kernel
+TARGET_KERNEL_CONFIG += vendor/oplus_yupik_QGKI.config
+
+# Partitions
+BOARD_ONEPLUS_DYNAMIC_PARTITIONS_SIZE := 10196353024 # BOARD_SUPER_PARTITION_SIZE - 4MB
+BOARD_SUPER_PARTITION_SIZE := 10200547328
 
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
@@ -22,4 +29,4 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 TARGET_RECOVERY_UI_MARGIN_HEIGHT := 103
 
 # Include the proprietary files BoardConfig.
-include vendor/oneplus/lemonade/BoardConfigVendor.mk
+include vendor/realme/lunaa/BoardConfigVendor.mk
