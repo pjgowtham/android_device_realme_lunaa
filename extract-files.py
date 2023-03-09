1bbd9de0ebe454867e47d65b1065d70db821e1a2
pjgowtham <pjgowtham@gmail.com>
2023-03-09 11:01:33 +0530

    lunaa: Initial rebrand for realme GT Master Edition
    * OTA assertion is matched with stock (RE54ABL1)
    * Set super partition size and group size to BOARD_SUPER_PARTITION_SIZE - 4MB s>
    * Include additional defconfig for oplus yupik
    
    Change-Id: If0bf43f9962b0a60f94e719b302db5e1b94497f7

--- extract-files.py
+++ extract-files.py
@@ -103,8 +103,8 @@ blob_fixups: blob_fixups_user_type = {
 }  # fmt: skip
 
 module = ExtractUtilsModule(
-    'lemonade',
-    'oneplus',
+    'lunaa',
+    'realme',
     namespace_imports=namespace_imports,
     blob_fixups=blob_fixups,
     lib_fixups=lib_fixups,
@@ -113,6 +113,6 @@ module = ExtractUtilsModule(
 
 if __name__ == '__main__':
     utils = ExtractUtils.device_with_common(
-        module, 'sm8350-common', module.vendor
+        module, '../oneplus/sm8350-common', module.vendor
     )
     utils.run()
