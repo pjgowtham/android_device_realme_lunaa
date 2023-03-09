234b594fddaaf0413d2f8db4d69fa4858fc5469e
pjgowtham <pjgowtham@gmail.com>
2023-03-09 11:01:33 +0530

    lunaa: Initial device tree conversion
    * OTA assertion is matched with stock (RE54ABL1)
    * Add VENDOR_COMMON to extraction scripts
    * Set super partition size and group size to BOARD_SUPER_PARTITION_SIZE - 4MB s>
    * Include additional defconfig for oplus yupik
    
    Change-Id: If0bf43f9962b0a60f94e719b302db5e1b94497f7

---
+++ lineage_lunaa.mk
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from lunaa device
+$(call inherit-product, device/realme/lunaa/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_lunaa
+PRODUCT_DEVICE := lunaa
+PRODUCT_MANUFACTURER := realme
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := RMX3360
+
+PRODUCT_SYSTEM_NAME := RE54ABL1
+PRODUCT_SYSTEM_DEVICE := RE54ABL1
+
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="RMX3360-user 13 TP1A.220905.001 R.136e3d6-af7c-10d436 release-keys" \
+    TARGET_DEVICE=$(PRODUCT_SYSTEM_NAME) \
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+
+BUILD_FINGERPRINT := realme/RMX3360/RE54ABL1:13/TP1A.220905.001/R.136e3d6-af7c-10d436:user/release-keys
