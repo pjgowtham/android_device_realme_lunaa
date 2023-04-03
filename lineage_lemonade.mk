0710e36f8f85e217cd58036aa08e08712ed135ae
Albert Tang <tangalbert919@yahoo.com>
2023-04-03 17:50:06 -0500

    lemonade: Update from OOS 11.F.21
    Change-Id: I8295f6637ad19a833ae8e95e4ee558bddc6c3f5e

--- lineage_lemonade.mk
+++ lineage_lemonade.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021-2022 The LineageOS Project
+# Copyright (C) 2021-2023 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -26,8 +26,8 @@ PRODUCT_SYSTEM_DEVICE := OnePlus9
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="OnePlus9-user 13 TP1A.220905.001 R.d81cce_5d491_5d538 release-keys" \
+    PRIVATE_BUILD_DESC="OnePlus9-user 13 TP1A.220905.001 R.ec5be2-5994_493 release-keys" \
     TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
-BUILD_FINGERPRINT := OnePlus/OnePlus9/OnePlus9:13/TP1A.220905.001/R.d81cce_5d491_5d538:user/release-keys
+BUILD_FINGERPRINT := OnePlus/OnePlus9/OnePlus9:13/TP1A.220905.001/R.ec5be2-5994_493:user/release-keys
