798918ff0c7c162cee391d003f554963a3f05ab8
pjgowtham <pjgowtham@gmail.com>
2023-10-18 16:28:23 +0530

    Revert "lemonade: Define proper charging control node"
    This reverts commit c26fac2ce0007fd3292dad0fcb56f210f7fb80b0.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,9 +15,6 @@ TARGET_SCREEN_DENSITY := 450
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
-# Lineage Health
-TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH := /sys/class/oplus_chg/battery/chg_enable
-
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
