aaa177d3e9d61cb220a37b5487f356c220ddb287
pjgowtham <pjgowtham@gmail.com>
2024-03-31 14:34:55 +0530

    lunaa: Build ALS correction service and configure grabrect

--- device.mk
+++ device.mk
@@ -51,6 +51,10 @@ PRODUCT_PACKAGES += \
     OPlusSettingsResTarget \
     OPlusSystemUIResTarget
 
+# Sensors
+PRODUCT_PACKAGES += \
+    vendor.lineage.oplus_als.service
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
